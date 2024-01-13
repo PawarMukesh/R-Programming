1ac187c97cac375497def2a94b86aed2b1522bff
Mukesh Pawar <mukeshpawar8793@gmail.com>
2024-01-13 11:43:15 +0530

    Add files via upload

---
+++ R-Basic/Type Casting -- Change the data type of variable.R
@@ -0,0 +1,23 @@
+x<-1L # Integer
+y<-2  # Numeric
+
+# Convert from integer to numeric
+a <- as.numeric(x)
+
+# Convert numeric to integer
+b <- as.integer(y)
+
+# Print the value x & y
+x
+y
+
+# print the class name a & b
+class(a)
+class(b)
+
+
+
+# Convert ineger to complex
+c <- as.complex(x)
+class(c)
+c
